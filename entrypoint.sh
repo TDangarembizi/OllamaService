49cb685b763d172719489262e99cdb11c99f3753
Tino <T0410828@my.ntu.ac.uk>
2025-04-24 21:25:04 +0100

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-# Start the server in background
-ollama serve &
-
-# Wait a bit for server to start
-sleep 5
 
 # Pull the model
 ollama pull llama3
 
-# Keep it running
-wait
+exec ollama serve
